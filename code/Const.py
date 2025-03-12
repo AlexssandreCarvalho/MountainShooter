abaaa475ffe4d7c752d0f7f60c307624f861da9c
Alexssandre Vieira Carvalho <alexvieiracarvalho@gmail.com>
2025-03-12 12:50:41 -0300

    Implementation SQLite3 database for Score Implementation Score using Proxy Design Pattern

--- code/Const.py
+++ code/Const.py
@@ -99,7 +99,7 @@ ENTITY_SPEED = {
     'Level2Bg3': 3,
     'Level2Bg4': 4,
     'Player1': 3,
-    'Player1Shot': 1,
+    'Player1Shot': 2,
     'Player2': 3,
     'Player2Shot': 3,
     'Enemy1': 1,
@@ -139,11 +139,30 @@ PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
 # S
 SPAWN_TIME = 4000  # 4 seconds
 
+# SCREEN POSITION
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
+
+SCORE_POSITION = {
+    'Title': (WIN_WIDTH / 2, 50),
+    'EnterName': (WIN_WIDTH / 2, 80),
+    'Label': (WIN_WIDTH / 2, 90),
+    'Name': (WIN_WIDTH / 2, 110),
+    0: (WIN_WIDTH / 2, 110),
+    1: (WIN_WIDTH / 2, 130),
+    2: (WIN_WIDTH / 2, 150),
+    3: (WIN_WIDTH / 2, 170),
+    4: (WIN_WIDTH / 2, 190),
+    5: (WIN_WIDTH / 2, 210),
+    6: (WIN_WIDTH / 2, 230),
+    7: (WIN_WIDTH / 2, 250),
+    8: (WIN_WIDTH / 2, 270),
+    9: (WIN_WIDTH / 2, 290),
+}
+
 # T
 TIMEOUT_LEVEL = 20000  # 20 seconds
 
 TIMEOUT_STEP = 100  # 100 milliseconds
 
-# W
-WIN_WIDTH = 576
-WIN_HEIGHT = 324
+
